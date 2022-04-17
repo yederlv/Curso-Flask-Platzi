db1513cf79851b848e16d377aa7436cf0be525a1
Yeder L Vicente <yedjovic17@gmail.com>
2022-04-17 00:13:06 -0500

    add: templates and static

--- yedi-flask/main.py
+++ yedi-flask/main.py
@@ -1,8 +1,22 @@
-from flask import Flask
+from glob import escape
+from flask import Flask, request, make_response, redirect, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
+def index():
+    user_ip = request.remote_addr
+
+    response = make_response(redirect('/hello'))
+    response.set_cookie('user_ip', user_ip)
+
+    return response
+
+
+@app.route('/hello')
 def hello():
-    return("Welcome to Yedi Flask App")
+    user_ip = request.cookies.get('user_ip')
+    user_ip = escape(user_ip)   # escape() is used to prevent XSS attacks
+    #return("Welcome to Yedi Flask App! Your IP is: {}".format(user_ip))
+    return render_template('hello.html', user_ip=user_ip)
